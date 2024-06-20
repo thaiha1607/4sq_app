af3daafa6d10a3f8f438e6b3483e17f5b1949f8b
thaiha1607 <halequocthai@outlook.com>
2024-06-20 08:46:31 +0700

    Revert "feat: Add Kafka and Cypress tests"
    This reverts commit efb0c46dc31ee6c17c324c5ab2150a609082c723.

--- src/main/java/com/foursquare/server/broker/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Spring cloud consumers and providers
- */
-package com.foursquare.server.broker;
--- src/test/java/com/foursquare/server/IntegrationTest.java
+++ src/test/java/com/foursquare/server/IntegrationTest.java
@@ -2,7 +2,6 @@ package com.foursquare.server;
 
 import com.foursquare.server.config.AsyncSyncConfiguration;
 import com.foursquare.server.config.EmbeddedElasticsearch;
-import com.foursquare.server.config.EmbeddedKafka;
 import com.foursquare.server.config.EmbeddedRedis;
 import com.foursquare.server.config.EmbeddedSQL;
 import com.foursquare.server.config.JacksonConfiguration;
@@ -21,6 +20,5 @@ import org.springframework.boot.test.context.SpringBootTest;
 @EmbeddedRedis
 @EmbeddedElasticsearch
 @EmbeddedSQL
-@EmbeddedKafka
 public @interface IntegrationTest {
 }
